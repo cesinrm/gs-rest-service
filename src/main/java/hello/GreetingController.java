ba6d64b723ae171433c109152ca92876fc03768a
Cesar Rodriguez Medina <Cesar.Rodriguez.Medina@ibm.com>
2018-01-04 00:12:18 +0100

    Update GreetingController.java
    e

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
-public class GreetingController {
+public class GreetingController { 
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
